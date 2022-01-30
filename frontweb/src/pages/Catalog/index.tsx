43e8fd6dfbd63f4bfd073490e24458a23d72866a
Augusto Nunes <augustocesar.udi@gmail.com>
2022-01-30 14:03:56 -0300

    Routes

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,46 +1,42 @@
-import Navbar from 'components/Navbar';
 import ProductCard from 'components/ProductCard';
 import Search from 'components/Search';
 
 const Catalog = () => {
   return (
-    <>
-      <Navbar />
-      <div className="container my-4">
-        <div className="base-card product-card">
-          <Search />
+    <div className="container my-4">
+      <div className="base-card product-card">
+        <Search />
+      </div>
+      <div className="row">
+        <div className="col-sm-6 col-lg-4">
+          <ProductCard />
         </div>
-        <div className="row">
-          <div className="col-sm-6 col-lg-4">
-            <ProductCard />
-          </div>
-          <div className="col-sm-6 col-lg-4">
-            <ProductCard />
-          </div>
-          <div className="col-sm-6 col-lg-4">
-            <ProductCard />
-          </div>
-          <div className="col-sm-6 col-lg-4">
-            <ProductCard />
-          </div>
-          <div className="col-sm-6 col-lg-4">
-            <ProductCard />
-          </div>
-          <div className="col-sm-6 col-lg-4">
-            <ProductCard />
-          </div>
-          <div className="col-sm-6 col-lg-4">
-            <ProductCard />
-          </div>
-          <div className="col-sm-6 col-lg-4">
-            <ProductCard />
-          </div>
-          <div className="col-sm-6 col-lg-4">
-            <ProductCard />
-          </div>
+        <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-lg-4">
+          <ProductCard />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,34 +1,31 @@
 import './styles.css';
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
-import Navbar from 'components/Navbar';
 import ButtonIcon from 'components/ButtonIcon';
+
 const Home = () => {
   return (
-    <>
-      <Navbar />
-      <div className="home-container">
-        <div className="base-card home-card">
-          <div className="home-content-container">
-            <div className="home-content-container-text">
-              <h1>O carro perfeito para você</h1>
-              <p>
-                Conheça nossos carros e dê mais um passo na realização do seu
-                sonho
-              </p>
-            </div>
-          </div>
-          <div className="home-image-container">
-            <MainImage />
+    <div className="home-container">
+      <div className="base-card home-card">
+        <div className="home-content-container">
+          <div className="home-content-container-text">
+            <h1>O carro perfeito para você</h1>
+            <p>
+              Conheça nossos carros e dê mais um passo na realização do seu
+              sonho
+            </p>
           </div>
         </div>
-        <div className="base-card home-card-btn">
-          <div className="home-btn-container">
-            <ButtonIcon name='VER CATÁLOGO'/>
-          </div>
-          <p>Comece agora a navegar</p>
+        <div className="home-image-container">
+          <MainImage />
         </div>
       </div>
-    </>
+      <div className="base-card home-card-btn">
+        <div className="home-btn-container">
+          <ButtonIcon name="VER CATÁLOGO" />
+        </div>
+        <p>Comece agora a navegar</p>
+      </div>
+    </div>
   );
 };
 
